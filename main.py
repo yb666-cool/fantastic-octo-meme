c1ce84ce3865b3ae1e548d3e04c6afb600e18dfb
yb666-cool <57516438+yb666-cool@users.noreply.github.com>
2023-03-20 18:32:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words.json()['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
